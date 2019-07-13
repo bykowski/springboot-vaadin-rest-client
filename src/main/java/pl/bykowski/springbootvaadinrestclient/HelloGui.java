1c0fd89fa3467bf8cf237ebe1ae1ee7bb766d964
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-07-13 10:14:32 +0200

    gif

--- src/main/java/pl/bykowski/springbootvaadinrestclient/HelloGui.java
+++ src/main/java/pl/bykowski/springbootvaadinrestclient/HelloGui.java
@@ -1,6 +1,7 @@
 package pl.bykowski.springbootvaadinrestclient;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -16,6 +17,12 @@ public class HelloGui extends VerticalLayout {
         TextField textFieldName = new TextField();
         Button buttonOk = new Button("witaj studencie", new Icon(VaadinIcon.ACADEMY_CAP));
         Label labelHello = new Label();
+
+        buttonOk.addClickListener(click -> {
+            labelHello.setText("Hello " + textFieldName.getValue());
+            Image image = new Image("https://media.tenor.com/images/1170597818a37a7c6e3e1d4baeb6e2eb/tenor.gif", "nie można pobrać obrazka");
+            add(image);
+        });
 
         add(textFieldName);
         add(buttonOk);
